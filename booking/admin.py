6b0780c4bc918128d75aaadc61b31bdb41a91976
daniel <daniel_kang09@hanmail.net>
2022-06-09 23:47:55 +0900

    어드민 사이트에서 Comment, Like를 Inline으로 관리할 수 있게함

--- booking/admin.py
+++ booking/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
 from .models import Branch, Booking, Comment, Like
 
+from django.contrib.contenttypes.admin import GenericStackedInline
 
 # Register your models here.
 
-admin.site.register(Branch)
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class LikeInline(GenericStackedInline):
+    model = Like
+
+
+# Branch에서 Comment 관리할 수 있게
+class BranchAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+# Comment에서 Like 관리할 수 있게
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
+
+
+admin.site.register(Branch, BranchAdmin)
 admin.site.register(Booking)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Like)
+
+
+
+
